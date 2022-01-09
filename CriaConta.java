69099addef8be30b7c23cfcf379a61ba2a06fc1c
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2022-01-09 12:12:54 -0300

    instanciação, atributos e referências, caracterisitcas do objeto, segunda instância, valores default de atributos, definindo valores de atributos.

--- CriaConta.java
+++ CriaConta.java
@@ -7,6 +7,14 @@ public class CriaConta {
         primeiraConta.saldo = 200;
 
         System.out.println(primeiraConta.saldo);
+        
+        primeiraConta.saldo += 100;
+
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 50;
+
+        System.out.println("A primeira conta tem saldo de R$ " + primeiraConta.saldo);
+        System.out.println("A segunda conta tem saldo de R$ " + segundaConta.saldo);
 
     }
 
