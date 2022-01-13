b6b41a017e08137442409faf1093426dfb88cb3e
Daniel Sonobe Silveira <dev.obe78@gmail.com>
2022-01-13 00:15:36 -0300

    método static

--- codeBank-encapsulado/Conta.java
+++ codeBank-encapsulado/Conta.java
@@ -5,14 +5,18 @@ class Conta {
 	private int agencia;
 	private int numero;
 	private Cliente titular;
+	private static int total;
 	
 	// Constructor >>
 	public Conta(int agencia, int numero) {
 		
+		Conta.total++;
+		//System.out.println("O total de contas é " + Conta.total);
+		
 		this.agencia = agencia;
 		this.numero = numero;
 		
-		System.out.println("Estou criando uma conta " + this.numero);
+		//System.out.println("Estou criando uma conta " + this.numero);
 		
 	}
 	
@@ -108,6 +112,12 @@ class Conta {
 	
 		return titular;
 	
+	}
+	
+	public static int getTotal() {
+		
+		return Conta.total;
+		
 	}
 	
 }
--- codeBank-encapsulado/TestaValores.java
+++ codeBank-encapsulado/TestaValores.java
@@ -1,12 +1,19 @@
 
 public class TestaValores {
-
-    public static void main(String[] args) {
-
-        Conta conta = new Conta(422, 1001);
-
-        System.out.println(conta.getAgencia());
-
-    } 
+	
+	public static void main(String[] args) {
+			
+		Conta conta = new Conta(422, 3001);
+		
+		System.out.println(conta.getAgencia());
+		
+		Conta conta2 = new Conta(322, 2001);
+		Conta conta3 = new Conta(211, 1001);		
+		
+		System.out.println(conta2.getAgencia());
+		System.out.println(conta3.getAgencia());
+				
+		System.out.println("O total de contas dentro da classe Conta é " + Conta.getTotal());
+	}
 
 }
